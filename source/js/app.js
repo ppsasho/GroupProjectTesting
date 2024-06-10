48029413a6fc35911e32ddbab1e9778695f4e5b8
TheForis <boriskrstovski@gmail.com>
2024-06-10 23:42:44 +0200

    11

---
+++ source/js/app.js
@@ -0,0 +1,8 @@
+let logoBtn = document.getElementById('logoBtn');
+let homeBtn = document.getElementById('homeBtn');
+let aboutBtn = document.getElementById('aboutBtn');
+let logInBtn = document.getElementById('logInBtn');
+let searchBtn = document.getElementById('searchBtn');
+let darkModeBtn = document.getElementById('darkModeBtn');
+
+
